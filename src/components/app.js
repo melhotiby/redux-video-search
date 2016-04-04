25a57e98dfa10776acebc221363453d8b21f1ef3
Matt Elhotiby <hotiby@gmail.com>
2016-04-04 08:37:31 -0400

    Initial redux implementation

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import SearchBar from '../containers/search_bar';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchVideos } from '../actions/fetch_videos'
+import _ from 'lodash';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { term: 'code', selectedVideo: null };
+    this.videoSearch = this.videoSearch.bind(this);
+    this.props.fetchVideos(this.state.term);
+  }
+
+  videoSearch(term){
+    this.setState({term: term });
+    this.props.fetchVideos(term);
+  }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
-      <SearchBar />
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+          <VideoDetail video={this.state.selectedVideo} />
+      </div>
     );
   }
 }
+
+function mapStateToProps({ videos }){
+  return { videos };
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchVideos }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers/videos.js
+++ src/reducers/videos.js
@@ -1,7 +1,6 @@
 import { FETCH_VIDEOS } from '../actions/fetch_videos';
 
 export default function(state = [], action){
-  console.log(action);
   switch (action.type) {
   case FETCH_VIDEOS:
      return [ action.payload.data, ...state ];
